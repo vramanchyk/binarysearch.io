957ac6413ece39719a52139a2546bd3d6368bc56
Vitali Ramanchyk <vitali.ramanchyk@gmail.com>
2020-07-31 22:47:17 -0400

    BS404 Oddly Specific

---
+++ src/com/github/vramanchyk/bs/BS404_Oddly_Specific.java
@@ -0,0 +1,11 @@
+package com.github.vramanchyk.bs;
+
+/**
+ * Created by Vitali Ramanchyk on 7/31/20.
+ */
+public class BS404_Oddly_Specific {
+
+    public int solve(int n) {
+        return (2 + (n - 1) * 2) * n / 2;
+    }
+}
